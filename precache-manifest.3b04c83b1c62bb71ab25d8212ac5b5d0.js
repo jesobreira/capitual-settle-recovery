e0f52bcfda014d393c5fac26726caf2416c973bd
jesobreira <jefrey.santos@capitual.io>
2021-01-17 15:06:57 -0300

    Updates

---
+++ precache-manifest.3b04c83b1c62bb71ab25d8212ac5b5d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333ed2286aa07f36a4dd9c3c9a4abf3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76845f7c9c841d3f4ea3",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "76845f7c9c841d3f4ea3",
+    "url": "/static/js/2.10084e28.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.10084e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44b216aa567b1d6fc47",
+    "url": "/static/js/main.1a4ae819.chunk.js"
+  },
+  {
+    "revision": "7669bae2eb150a59dd0f",
+    "url": "/static/js/runtime-main.fc859078.js"
+  },
+  {
+    "revision": "ac1587f2727cd8e192609da886c2d6d7",
+    "url": "/static/media/ordem-saque.ac1587f2.svg"
+  }
+]);
